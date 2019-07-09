42a19a041ac9630ddebe5465fd698177130c3132
Pierce Franklin <piush_r@hotmail.com>
2019-07-09 15:38:04 +0900

    added journal controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'journals/index'
+  get 'journals/show'
+  get 'journals/new'
+  get 'journals/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_025225) do
+ActiveRecord::Schema.define(version: 2019_07_09_060642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_025225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "title"
     t.index ["user_id"], name: "index_journals_on_user_id"
   end
 
