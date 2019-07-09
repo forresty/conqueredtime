19e4739d97b0c44c29a0c630c3f5a17defe5ec0d
Steven Tegnelia <stegnelia@gmail.com>
2019-07-09 14:28:22 +0900

    forrest approved

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-   # resources :journals, only: [:show, :index]
-   #  resources :entries, only: [:show, :index, :new, :create]
-   # end
+  resources :journals, only: [:show, :index, :new, :create] do
+    resources :entries, only: [:index, :new, :create]
+  end
 
-   resources :pomodoros, only: [:new, :create, :show]
 
-  # resources :entries, only: [:show, :index, :new, :create, :update]
-  #   resources :pomodoros, only: [:create, :new, :show]
-  # end
+  resources :milestones do
+    resources :pomodoros, only: [:new, :create]
+  end
+
+
+  resources :pomodoros, only: [:show]
+  resource :profiles, only: [:show, :edit, :update]
+  resources :entries, only: [:show]
+
+
 
    # resources :profile, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
