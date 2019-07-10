e392bbf53ff0508bdc2dc2712404c1c68948c2fd
Lena Kordbacheh <lena.ryzhova@yahoo.com>
2019-07-10 15:27:54 +0900

    red button to pomodoro

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,6 +5,7 @@ class EntriesController < ApplicationController
     @entries = @journal.entries
     @entry = Entry.new
     @entry.milestones.build
+    # @pomodoro = Pomodoro.new
   end
 
   def show
